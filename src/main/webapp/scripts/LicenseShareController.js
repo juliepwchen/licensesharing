4030bb2d2763239a5ba42eb5125acd10ea479e17
juliechen <juliechen@gmail.com>
2017-12-12 20:37:56 -0800

    Testing countdown clock.

--- src/main/webapp/scripts/LicenseShareController.js
+++ src/main/webapp/scripts/LicenseShareController.js
@@ -1,19 +1,4 @@
 var app = angular.module('licenseSharingApp');
 app.controller("LicenseShareController", ['$scope', '$rootScope', '$http', function(sc, rs, hp) {
-    sc.isLogin = "";
-	sc.login = function () {
 
-    	hp.get("http://jbossews-hybridteam.rhcloud.com/login?"+"username=" + sc.EmailAddress + "&password=" + sc.Password )
-        .success(function (response) {
-          sc.isLogin = response.IsAuthenicated;
-
-      	  if (sc.isLogin == "true") {
-      	    window.location.replace("/public/applicationMasterPage.html#/hybridteam");
-          } else {
-          	alert ("Re-enter User Name and Pass-Word.")
-          }
-
-        });
-
-    };
 }]);
--- src/main/webapp/scripts/MainController.js
+++ src/main/webapp/scripts/MainController.js
@@ -1,5 +1,5 @@
 var app = angular.module('licenseSharingApp');
-app.controller("LicenseShareController", ['$scope', '$rootScope', '$http', function(sc, rs, hp) {
+app.controller("MainController", ['$scope', '$rootScope', '$http', function(sc, rs, hp) {
     $scope.ChangeView = function () {
         $location.url('/licenseshare');
     }
