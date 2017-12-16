8d3e58f47bf5a5d66a761cc7c91a61a2e13448c1
juliechen <juliechen@gmail.com>
2017-12-16 15:23:35 -0800

    Testing Microsoft Graph API Connect for SPA.

--- src/main/webapp/scripts/msalconfig.js
+++ src/main/webapp/scripts/msalconfig.js
@@ -1,5 +1,5 @@
 ﻿var msalconfig = {
-    clientID: "6034f470-79b2-4043-a51a-343c65c57009",
+    clientID: "5de99563-4eec-4cc6-959c-8aa4a97d0d69",
     redirectUri: "http://licensesharing-licensesharing.b9ad.pro-us-east-1.openshiftapps.com/html/base.html#!/license"
     //redirectUri: location.origin
 };
